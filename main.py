07a2b992eda8c17cb96615f23d626f06c4e27fbe
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2023-04-13 15:39:35 +0200

    Init

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from utils.SimpleProjectHandler import SimpleProjectHandler
 
 def run_main():
     project_handler: IProjectHandler = SimpleProjectHandler(
-        project_title="Example Project",
-        file_path_project="/Users/maximilianbundscherer/Desktop/test/",
+        file_path_root="/Users/maximilianbundscherer/Desktop/test/",
+        file_path_data="",
+        project_title="My Project",
+        project_version_tag="v0",
         logging_level=logging.DEBUG
     )
+
     project_handler.init_project()
 
     project_handler.log_info("Hello World")
