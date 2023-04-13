8f82c46075680b8a02cb09756a473d3a5a1b0598
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2023-04-13 17:17:38 +0200

    Init

--- stages/ExampleStage0.py
+++ stages/ExampleStage0.py
@@ -4,4 +4,8 @@ from stages.IStage import IStage
 class ExampleStage0(IStage):
 
     def run_stage(self, example_str: str):
-        self.log_info("Hello World " + example_str + " " + self.get_file_path_cache())
+        self.log_info("Hello World " + example_str)
+        # self.log_space()
+        self.log_debug("Data Path: " + self.get_file_path_data("test.txt"))
+        self.log_debug("Out Path: " + self.get_file_path_out("test.txt"))
+        self.log_debug("Cache Path: " + self.get_file_path_cache("test.txt"))
