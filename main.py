68daa3fa22406dd1e9b3ce7c76911bee387fc100
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2023-04-13 15:47:31 +0200

    Init

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import logging
+# import pathlib
 
 from utils import IProjectHandler
 from utils.SimpleProjectHandler import SimpleProjectHandler
 
 
 def run_main():
+    # file_path_root = str(pathlib.Path().resolve()) + "/test/"  # Get current working directory
+    file_path_root = "/Users/maximilianbundscherer/Desktop/test/"  # Set path manually
+
     project_handler: IProjectHandler = SimpleProjectHandler(
-        file_path_root="/Users/maximilianbundscherer/Desktop/test/",
+        file_path_root=file_path_root,
         file_path_data="",
         project_title="My Project",
         project_version_tag="v0",
