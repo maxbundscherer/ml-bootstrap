823ce7a6f612d8ab478b0eddf6384bc0cf28a886
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2023-04-16 20:31:01 +0200

    Init

--- framework/Config.py
+++ framework/Config.py
@@ -1,16 +1,14 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class PathConfig:
-    def __init__(self,
-                 file_path_data: str,
-                 file_path_out: str,
-                 file_path_cache: str):
-        self.file_path_data: str = file_path_data
-        self.file_path_out: str = file_path_out
-        self.file_path_cache: str = file_path_cache
+    file_path_data: str
+    file_path_out: str
+    file_path_cache: str
 
 
+@dataclass
 class LoggingConfig:
-    def __init__(self,
-                 level: int,
-                 hide_prefix: bool):
-        self.level: int = level
-        self.hide_prefix: bool = hide_prefix
+    level: int
+    hide_prefix: bool
