24c6edb331053ceb100931ce3e188aded3abfc06
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2023-04-18 16:03:19 +0200

    Add CacheHelperTests

--- stages/ExampleStage001.py
+++ stages/ExampleStage001.py
@@ -1,58 +1,59 @@
-import os
 from dataclasses import dataclass
 
+from framework.CacheHelper import FileCacheHelper
 from framework.Stage import Stage
 from framework.Context import Context, SummaryText
 
 
 @dataclass
 class ExampleStage001Input:
-    pass
+    test_message: str
 
 
 @dataclass
 class ExampleStage001Config:
-    test_file_name: str
+    test_message_suffix: str
 
 
 @dataclass
 class ExampleStage001Output:
-    test_file_path: str
+    message: str
 
 
 class ExampleStage001(Stage[ExampleStage001Input, ExampleStage001Config, ExampleStage001Output]):
 
     @staticmethod
     def _preview(context: Context, inp: ExampleStage001Input, conf: ExampleStage001Config):
-        context.log_info("Got name '" + conf.test_file_name + "'")
+        context.log_debug("Should append '" + inp.test_message + "' with '" + conf.test_message_suffix + "'")
 
     @staticmethod
     def _get_cached(context: Context, inp: ExampleStage001Input, conf: ExampleStage001Config) -> ExampleStage001Output:
-        f = context.get_file_path_cache(conf.test_file_name)
+        content: str = FileCacheHelper(context=context, file_name="testfile.txt").read_cache()
 
-        if os.path.exists(f):
+        if content is not None:
             return ExampleStage001Output(
-                test_file_path=f
+                message=content
             )
 
     @staticmethod
     def _process(context: Context, inp: ExampleStage001Input, conf: ExampleStage001Config) -> ExampleStage001Output:
-        f = context.get_file_path_cache(conf.test_file_name)
+        content: str = inp.test_message + conf.test_message_suffix
 
-        context.log_info("This is a Test. Write to " + f)
-
-        with open(f, "w") as file:
-            file.write("Hello World!")
+        context.log_debug("My content is '" + content + "'")
 
         # context.log_debug("Data path: " + context.get_file_path_data())
         # context.log_debug("Cache path: " + context.get_file_path_cache())
         # context.log_debug("Out path: " + context.get_file_path_out())
 
         return ExampleStage001Output(
-            test_file_path=f
+            message=content
         )
 
     @staticmethod
+    def _write_cache(context: Context, out: ExampleStage001Output, conf: ExampleStage001Config):
+        FileCacheHelper(context=context, file_name="testfile.txt").write_cache(out.message)
+
+    @staticmethod
     def _after_process(context: Context, out: ExampleStage001Output, conf: ExampleStage001Config):
-        context.log_info("File written to " + out.test_file_path)
+        context.log_debug("Got content '" + out.message + "'")
         context.summary_add(SummaryText(message="Test Summary"))
--- stages/ExampleStage002.py
+++ stages/ExampleStage002.py
@@ -23,28 +23,18 @@ class ExampleStage002Output:
 class ExampleStage002(Stage[ExampleStage002Input, ExampleStage002Config, ExampleStage002Output]):
 
     @staticmethod
-    def _preview(context: Context, inp: ExampleStage002Input, conf: ExampleStage002Config):
-        pass
-
-    @staticmethod
-    def _get_cached(context: Context, inp: ExampleStage002Input, conf: ExampleStage002Config) -> ExampleStage002Output:
-        pass
-
-    @staticmethod
     def _process(context: Context, inp: ExampleStage002Input, conf: ExampleStage002Config) -> ExampleStage002Output:
-        f = inp.stage_001_out.test_file_path
-        m = ""
-        with open(f, "r") as file:
-            m = file.read()
-            context.log_info("This is a Test Read from " + f + ": " + m)
+        content = inp.stage_001_out.message
 
-        context.log_debug("Debug message 123")
+        context.log_debug("Got message '" + content + "'")
         # context.log_debug("Data path: " + context.get_file_path_data())
         # context.log_debug("Cache path: " + context.get_file_path_cache())
         # context.log_debug("Out path: " + context.get_file_path_out())
 
-        return ExampleStage002Output(test_message=m)
+        content = content + "!"
+
+        return ExampleStage002Output(test_message=content)
 
     @staticmethod
-    def _after_process(context: Context, out: ExampleStage001Output, conf: ExampleStage002Config):
-        context.log_info("Done")
+    def _after_process(context: Context, out: ExampleStage002Output, conf: ExampleStage002Config):
+        context.log_debug("Got final message '" + out.test_message + "'")
